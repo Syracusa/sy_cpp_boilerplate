7a6fa070c2580eb4dbd7cb454f4b40735eb2e4f6
Syracusa <msrndsy@gmail.com>
2022-10-20 09:18:31 +0900

    Wordcount first commit

--- examples/ex_stl_map.cpp
+++ examples/ex_stl_map.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <unordered_map>
 
+std::unordered_map<std::string, int> umap = {};
 
 const char* text = "Lorem ipsum dolor sit amet,"
 " consectetur adipiscing elit, sed do eiusmod tempor"
@@ -33,8 +34,45 @@ static void test()
     #endif
 }
 
+#define MAX_WORD_LEN 100
+static void count()
+{
+    int pos = 0;
+    int wpos = 0;
+    char wordbuf[MAX_WORD_LEN];
+
+    while(1)
+    {
+        char c = text[pos];
+        pos++;
+
+        if (c == '\0')
+            break;    
+
+        if (c == ' '){
+            if (wpos != 0){
+                wordbuf[wpos] = '\0';
+                if (umap[wordbuf]){
+                    umap[wordbuf] += 1;
+                } else {
+                    umap[wordbuf] = 1;
+                }
+                std::cout << wordbuf << " " << umap[wordbuf] << std::endl;
+            }
+            wpos = 0;
+        } else {
+            wordbuf[wpos] = c; 
+            wpos++;
+        }
+
+        // putchar(c);
+    }
+
+}
+
 int main()
 {
     // std::unordered_map<std::string, int> umap = {};
-    test();
+    // test();
+    count();
 }
