6217e6a2a5cfc84929ecd3ee1fe83ba711ce9ea7
Syracusa <msrndsy@gmail.com>
2022-10-28 09:03:28 +0900

    Time resolution ms to us

--- include/performance.h
+++ include/performance.h
@@ -4,18 +4,18 @@
 
 #include <time.h>
 
-static inline uint64_t _time_ms()
+static inline uint64_t _time_us()
 {
     struct timespec ts;
     clock_gettime(0 /* CLOCK_REALTIME */, &ts);
     uint64_t res = 0;
-    res += ts.tv_sec * 1000;
-    res += ts.tv_nsec / 1000000;
+    res += ts.tv_sec * 1000000;
+    res += ts.tv_nsec / 1000;
 
     return res;
 }
 
-#define TIME_MEASURE_START(n) uint64_t _befms_ ## n =  _time_ms()
-#define TIME_MEASURE_END(n) fprintf(stderr, #n " : %lums elapsed\n", _time_ms() - _befms_ ## n)
+#define TIME_MEASURE_START(n) uint64_t _befus_ ## n =  _time_us()
+#define TIME_MEASURE_END(n) fprintf(stderr, #n " : %luus elapsed\n", _time_us() - _befus_ ## n)
 
 #endif
